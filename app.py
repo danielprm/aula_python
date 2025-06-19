822c5c75e473b5090785b94a657823f86f4277c1
daniel <danielprmjob@gmail.com>
2025-06-19 18:05:40 -0300

    inserindo opções

---
+++ app.py
@@ -0,0 +1,10 @@
+print('sabor express\n')
+
+
+print('1. cadastrar restaurante')
+print('2. listar restaurante')
+print('3. ativar restaurante')
+print('4. sair\n')
+
+opção_escolhida = input('escolha uma opção: ')
+print('voce escolheu a opção', opção_escolhida)

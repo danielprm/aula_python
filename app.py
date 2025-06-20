bbb8d81002de964e788486d9102fd2c5001a5bcd
daniel <danielprmjob@gmail.com>
2025-06-19 23:22:31 -0300

    if else no python

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ print('2. listar restaurante')
 print('3. ativar restaurante')
 print('4. sair\n')
 
-opção_escolhida = input('escolha uma opção: ')
-print(f'voce escolheu a opção{opção_escolhida}')
+opção_escolhida = int(input('escolha uma opção: '))
+#print(f'voce escolheu a opção{opção_escolhida}')
+#opção_escolhida = int(opção_escolhida)
+
+if opção_escolhida == 1:
+    print('cadastrar restaurante')
+elif opção_escolhida == 2:
+    print('listar restaurantes')
+elif opção_escolhida == 3:
+    print('ativar restaurante')
+else:
+    print('encerrando o programa.')
--- exercicio1.py
+++ exercicio1.py
@@ -10,4 +10,6 @@ A
 ''')
 
 pi=3.14159
-print(f'o valor arredondado de pi é: {round(pi)}')
+print('O valor arredondado de pi é:', round(pi, 2))
+print(f'o valor arredondado de pi é: {pi:.2f}')
+print('O valor arredondado de pi é: {:.2f}'.format(pi))
